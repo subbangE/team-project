6e82c7d99e0b7dbe6b8887f22537b6c95cb487e9
SUBIN <ksb010105@naver.com>
2024-05-27 17:32:00 +0900

    fix application.properties line 10

--- src/main/java/com/myapp/team/Board/Question.java
+++ src/main/java/com/myapp/team/Board/Question.java
@@ -1,5 +1,16 @@
 package com.myapp.team.Board;
 
-public class Question {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+public class Question {
+    private Integer question_no;
+
+    private String question_title;
+
+    private String question_content;
 }
