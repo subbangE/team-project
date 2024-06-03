c96ba502003de71c005ed3e6f57823c38b36f0dd
Moon Young Doon <jo94hk@gamil.com>
2024-06-03 17:28:02 +0900

    feat: GetMapping getrole

--- src/main/java/com/myapp/team/role/RoleController.java
+++ src/main/java/com/myapp/team/role/RoleController.java
@@ -1,4 +1,18 @@
 package com.myapp.team.role;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class RoleController {
+    //roleMapper 선언하고 생성자 주입
+
+    @GetMapping("/users/{ id }")
+    public Role getRole(@PathVariable("id") String id) {
+        Role role = RoleMapper.getRole(id);
+        return role;
+
+
+    }
 }
--- src/main/java/com/myapp/team/role/RoleMapper.java
+++ src/main/java/com/myapp/team/role/RoleMapper.java
@@ -1,4 +1,11 @@
 package com.myapp.team.role;
 
-public class RoleMapper {
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface RoleMapper {
+    @Select("select * from user where id=#{id}")
+    Role getRole(@Param("id") String id);
 }
